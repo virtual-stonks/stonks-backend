f9e5de53a94354ac91a2192719abf61535368216
Sabuj Jana <sabujlovescoding@gmail.com>
2021-03-07 18:08:45 +0530

    setting up express

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser =  require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/', (req, res) => {
+    res.send('stonks-backend is running!');
+})
+
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
