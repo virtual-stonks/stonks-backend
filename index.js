18d6b9438f19e12de50aadd5a199d2ef1ecfdcea
Sabuj Jana <sabujlovescoding@gmail.com>
2021-05-22 12:08:01 +0530

    heroku commit

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
   .then(() => app.listen(PORT, () => {
-  	console.log(`Server Running on Port: ${PORT}`, process.env.CRON_TIME)   
+  	console.log(`Server Running on Port: ${PORT}`)   
   }))
   .catch((error) => console.log(`${error} did not connect`));
 
